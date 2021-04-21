55eabde5a45fc8d88b24db8f8d16197285b053d7
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-04-21 12:33:25 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import os
+
+    API_ID = int(os.environ.get("API_ID", 0))
+    API_HASH = os.environ.get("API_HASH", None)
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    APP_NAME = os.environ.get("APP_NAME", None)
+    API_KEY = os.environ.get("API_KEY", None)
+    APP_ID = os.environ.get("APP_ID")
+     
+
